f83e60b2b6766b3d427b9e2e66406cf425e8bafb
hoangminhphu188 <105615336+hoangminhphu188@users.noreply.github.com>
2022-05-23 00:14:37 +0700

    Update web.php
    phu: tao moi su kien va luu

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::middleware([CheckAdmin::class])->group(function () { // ham kiem tra admi
     Route::get('admin/events/search', [EventController::class, 'search'])->name('admin.events.search');// yeu cau tim kiem su kien
 
     Route::get('/admin/events/create', [EventController::class, 'create'])->name('admin.events.create');//yeu cau tao moi su kien
-    Route::post('/admin/events/create/process', [EventController::class, 'store'])->name('admin.events.create.process');
+    Route::post('/admin/events/create/process', [EventController::class, 'store'])->name('admin.events.create.process'); // tao moi su kien va luu 
 
     Route::post('/admin/events/delete', [EventController::class, 'delete'])->name('admin.events.delete');
 
