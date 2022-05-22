6f247e566d8ac0098ea3564ae4b261138f7b5fb0
hoangminhphu188 <105615336+hoangminhphu188@users.noreply.github.com>
2022-05-23 00:41:17 +0700

    Update create.blade.php
    phu: form tao moi event

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -25,7 +25,7 @@
                     </div>
                     @endif
                     <form method="post" action="{{ route('admin.events.create.process') }}"
-                        enctype="multipart/form-data">
+                        enctype="multipart/form-data"> // from du lieu tao moi event
                         @csrf
                         <div class="row">
                             <div class="col-md-5 col-sm-5">
